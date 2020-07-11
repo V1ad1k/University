10cce95d6c2e26db8f461867b58daa32f241d19e
Vladyslav Gavryliuk <58902929+V1ad1k@users.noreply.github.com>
2020-07-11 19:32:31 +0200

    Add files via upload

---
+++ DSaA/Lab10/DisjointSetDataStructure.java
@@ -0,0 +1,12 @@
+package Lab10;
+
+public interface DisjointSetDataStructure
+{
+    void makeSet(int item);
+
+    int findSet(int item);
+
+    boolean union(int itemA, int itemB);
+
+    int countSets();
+}
